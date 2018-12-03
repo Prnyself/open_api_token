7ce64ccceb7a4c0d40e64802fd07140c82701484
Prnyself <281239768@qq.com>
2018-12-03 14:48:59 +0800

    添加model方法

---
+++ model/api_message.go
@@ -0,0 +1,21 @@
+package model
+
+type ApiMessage struct {
+	Code        int
+	Name        string
+	Description string
+}
+
+func GetMessageByCode(code int) map[string]interface{} {
+	msg := ApiMessage{}
+	db.Where(&ApiMessage{Code: code}).First(&msg)
+	return msg.toHash()
+}
+
+func (apiMessage *ApiMessage) toHash() map[string]interface{} {
+	res := make(map[string]interface{}, 3)
+	res["code"] = apiMessage.Code
+	res["message"] = apiMessage.Name
+	res["description"] = apiMessage.Description
+	return res
+}
--- model/campus_app.go
+++ model/campus_app.go
@@ -1,2 +1,19 @@
 package model
 
+type CampusApp struct {
+	Id        int
+	AppKey    string
+	AppSecret string
+}
+
+func GetAppByKeySecret(key, secret string) CampusApp {
+	var app CampusApp
+	db.First(&app, &CampusApp{AppKey: key, AppSecret: secret})
+	return app
+}
+
+func GetAppById(id int) CampusApp {
+	var app CampusApp
+	db.First(&app, id)
+	return app
+}
